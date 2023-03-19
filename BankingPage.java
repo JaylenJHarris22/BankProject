145f15a3fda2e09026030de95e2481224ab6ca86
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-03-19 12:34:14 -0500

    Worked on the structure for the program

--- BankingPage.java
+++ BankingPage.java
@@ -7,6 +7,7 @@ public class BankingPage {
         System.out.println("Welcome to National Bank of Jaylen!");
         System.out.println("If you would like to register please enter R. Otherwise if you would like to quit press Q.");
         String choice = "";
+        boolean proceed;
 
 //        try {
 //            choice = scanner.nextLine();
@@ -17,14 +18,37 @@ public class BankingPage {
         do {
             choice = scanner.next();
             choice = choice.toUpperCase();
-            if (!choice.equals("R") || choice.equals("Q")) {
+            if (!choice.equals("R") || !choice.equals("Q")) {
                 System.out.println("Please enter a R to register or Q to quit.");
             }
-        }while (!choice.equals("R") || choice.equals("Q"));
+        }while (!choice.equals("R") && !choice.equals("Q"));
 
         if (choice.equals("R")){
             System.out.println("Thank you for choosing National Bank of Jaylen. We appreciate your business.");
-            System.out.println("Since you have proceeded to re");
+            System.out.println("Since you have proceeded to register with our great bank we need to get information to a set up " +
+                    "your profile.");
+            proceed = true;
+        }else if(choice.equals("Q")){
+            System.out.println("Press q once more if you are sure you would not like to register otherwise press r to proceed.");
+
+            do {
+                choice = scanner.next();
+                choice = choice.toUpperCase();
+                if (!choice.equals("R") && !choice.equals("Q")) {
+                    System.out.println("Please enter a R to register or Q to quit.");
+                }
+            }while (!choice.equals("R") && !choice.equals("Q"));
+
+            if (choice.equals("R")){
+                System.out.println("Thank you for choosing National Bank of Jaylen. We appreciate your business.");
+                System.out.println("Since you have proceeded to register with our great bank we need to get information to a set up " +
+                        "your profile.");
+                proceed = true;
+            }else if (choice.equals("Q")){
+                System.out.println("Have a wonderful day!");
+            }
         }
+
+
     }
 }
